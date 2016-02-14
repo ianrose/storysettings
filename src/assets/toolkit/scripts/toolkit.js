555f473bb84519e9590299739e85467802c5e9f1
Ian Rose <ian@ianrose.me>
2016-02-14 18:43:45 -0500

    building

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -2,8 +2,6 @@
  * Toolkit JavaScript
  */
 
- /* globals $ */
-
  // Returns a function, that, as long as it continues to be invoked, will not
  // be triggered. The function will be called after it stops being called for
  // N milliseconds. If `immediate` is passed, trigger the function on the
